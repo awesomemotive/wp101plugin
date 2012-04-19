cf4c9ffc204147ea08a39c57161b1636aabad8e8
Shawn Hesketh <shawn@leftlanedesigns.com>
2012-04-19 11:26:02 -0500

    Placeholder for Embed Code
    Added an example of the video embed code within a placeholder for the "Embed Code" field (wp101_video_code), to assist folks who might want to match their video size to the WP101 videos.
    
    Signed-off-by: Shawn Hesketh <shawn@leftlanedesigns.com>

--- wp101.php
+++ wp101.php
@@ -380,7 +380,7 @@ class WP101_Plugin {
 		</tr>
 		<tr valign="top">
 			<th scope="row"><label for="wp101-video-code">Embed Code:</label></th>
-			<td><textarea rows="5" cols="50" id="wp101-video-code" name="wp101_video_code" class="large-text"><?php echo $editable_video ? esc_textarea( $editable_video['content'] ) : ''; ?></textarea></td>
+			<td><textarea rows="5" cols="50" id="wp101-video-code" name="wp101_video_code" class="large-text" placeholder="Example: <iframe src='http://player.vimeo.com/video/33767000' width='640' height='360' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>"><?php echo $editable_video ? esc_textarea( $editable_video['content'] ) : ''; ?></textarea></td>
 		</tr>
 		</table>
 		<?php
