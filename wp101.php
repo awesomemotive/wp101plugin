86c106d0029a493eb23d7b83ae72f7f14c5953c2
Shawn Hesketh <shawn@leftlanedesigns.com>
2013-06-24 17:34:03 -0500

    Version bump.

--- wp101.php
+++ wp101.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP101
 Description: WordPress tutorial videos, delivered directly in the WordPress dashboard.
-Version: 2.0.4
+Version: 2.0.5
 Author: WP101Plugin.com
 Author URI: http://wp101plugin.com/
 */
