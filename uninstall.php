736f1b4425ed2a00a88e33413674556bc461ef43
Brandon Hubbard <bkhubbard@gmail.com>
2015-10-14 11:03:58 -0700

    Added uninstall.php
    Now when you uninstall the plugin it will remove the p101 options and
    transients.

---
+++ uninstall.php
@@ -0,0 +1,21 @@
+<?php
+
+// If uninstall is not called from WordPress, exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit();
+}
+
+// Delete Options
+delete_option('wp101_api_key');
+delete_option('wp101_db_version');
+delete_option('wp101_hidden_topics');
+delete_option('wp101_custom_topics');
+
+// Delete Transients
+delete_transient( 'wp101_topics' );
+delete_transient(  'wp101_wpseo_topics' );
+delete_transient(  'wp101_message' );
+delete_transient(  'wp101_get_admin_count' );
+delete_transient(  'wp101_api_key_valid' );
+
+
