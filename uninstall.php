191907a88dc885a4d54de21dba88d0d2458a5fbe
Brandon Hubbard <bkhubbard@gmail.com>
2015-10-14 11:24:00 -0700

    Fixed spacing on uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,21 +2,19 @@
 
 // If uninstall is not called from WordPress, exit
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit();
+	exit();
 }
 
 // Delete Options
-delete_option('wp101_api_key');
-delete_option('wp101_db_version');
-delete_option('wp101_hidden_topics');
-delete_option('wp101_custom_topics');
-delete_option('wp101_admin_restriction');
+delete_option( 'wp101_api_key' );
+delete_option( 'wp101_db_version' );
+delete_option( 'wp101_hidden_topics' );
+delete_option( 'wp101_custom_topics' );
+delete_option( 'wp101_admin_restriction' );
 
 // Delete Transients
 delete_transient( 'wp101_topics' );
-delete_transient(  'wp101_wpseo_topics' );
-delete_transient(  'wp101_message' );
-delete_transient(  'wp101_get_admin_count' );
-delete_transient(  'wp101_api_key_valid' );
-
-
+delete_transient( 'wp101_wpseo_topics' );
+delete_transient( 'wp101_message' );
+delete_transient( 'wp101_get_admin_count' );
+delete_transient( 'wp101_api_key_valid' );
