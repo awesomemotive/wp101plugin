f784e80ded9cdfc8c004cb430176423c8b4261d5
Steve Grunwell <sgrunwell@liquidweb.com>
2018-03-05 16:55:06 +0000

    Upon plugin activation, display a single notification with a link to the WP101 page for an available series

--- includes/addons.php
+++ includes/addons.php
@@ -7,6 +7,7 @@
 
 namespace WP101\Addons;
 
+use WP101\Admin as Admin;
 use WP101\TemplateTags as TemplateTags;
 
 /**
@@ -36,3 +37,68 @@ function check_plugins() {
 	update_option( 'wp101-available-series', $available, false );
 }
 add_action( 'activated_plugin', __NAMESPACE__ . '\check_plugins' );
+
+/**
+ * In the administration area, alert users who are capable of purchasing add-ons to any new series
+ * that may be of interest.
+ *
+ * @param WP_Screen $screen The current WP admin screen.
+ */
+function show_notifications( $screen ) {
+	$screens = [ 'plugins', 'toplevel_page_wp101', 'video-tutorials_page_wp101-settings' ];
+
+	if ( ! in_array( $screen->id, $screens, true ) || ! current_user_can( Admin\get_addon_capability() ) ) {
+		return;
+	}
+
+	$available = get_option( 'wp101-available-series', [] );
+	$dismissed = (array) get_user_meta( get_current_user_id(), 'wp101-dismissed-notifications', true );
+
+	// Abort if we have nothing to say.
+	if ( empty( $available ) ) {
+		return;
+	}
+
+	// Register the callback to render the notification.
+	add_action( 'admin_notices', function () use ( $available ) {
+		$links = [];
+
+		foreach ( (array) $available as $addon ) {
+			$links[] = sprintf( '<strong><a href="%1$s" target="_blank">%2$s</a></strong>', $addon['url'], $addon['title'] );
+		}
+
+		// Flatten the $links array into a single string.
+		if ( 1 === count( $links ) ) {
+			$link = array_shift( $links );
+		} else {
+			$and  = array_pop( $links );
+			$link = implode( _x( ', ', 'separator for multiple series in a sentence', 'wp101' ), $links );
+			if ( 2 <= count( $links ) ) {
+				$link .= _x( ', ', 'Oxford comma', 'wp101' );
+			}
+			$link .= _x( 'and ', 'separator between the last two items in a list', 'wp101' ) . $and;
+		}
+
+		render_notification( sprintf(
+			/* Translators: %1$s is the add-on title(s). */
+			__( 'Get the most out of your site with %1$s from WP101.', 'wp101' ),
+			$link
+		) );
+	} );
+}
+add_action( 'current_screen', __NAMESPACE__ . '\show_notifications' );
+
+/**
+ * Render a notification based on the WordPress standards.
+ *
+ * @param string $message The unescaped message contents.
+ */
+function render_notification( $message ) {
+?>
+
+	<div class="notice notice-info is-dismissible">
+		<p><?php echo wp_kses_post( $message ); ?></p>
+	</div>
+
+<?php
+}
--- tests/test-addons.php
+++ tests/test-addons.php
@@ -7,6 +7,7 @@
 
 namespace WP101\Tests;
 
+use WP_Screen;
 use WP101\Addons as Addons;
 
 /**
@@ -23,7 +24,7 @@ class AddonTest extends TestCase {
 		$api = $this->mock_api();
 		$api->shouldReceive( 'get_addons' )
 			->once()
-			->andReturn([
+			->andReturn( [
 				'addons' => [
 					[
 						'title'                  => 'Learning Some Plugin',
@@ -36,11 +37,11 @@ class AddonTest extends TestCase {
 						],
 					],
 				],
-			]);
+			] );
 
 		Addons\check_plugins();
 
-		$this->assertEquals([
+		$this->assertEquals( [
 			[
 				'title'  => 'Learning Some Plugin',
 				'url'    => 'https://wp101plugin.com/series/some-plugin',
@@ -56,7 +57,7 @@ class AddonTest extends TestCase {
 
 		$api = $this->mock_api();
 		$api->shouldReceive( 'get_addons' )
-			->andReturn([
+			->andReturn( [
 				'addons' => [
 					[
 						'title'                  => 'Learning Some Plugin',
@@ -69,10 +70,128 @@ class AddonTest extends TestCase {
 						],
 					],
 				],
-			]);
+			] );
 
 		Addons\check_plugins();
 
 		$this->assertEmpty( get_option( 'wp101-available-series', [] ) );
+	}
+
+	public function test_show_notifications() {
+		wp_set_current_user( $this->factory()->user->create( [
+			'role' => 'administrator',
+		] ) );
+
+		update_option( 'wp101-available-series', [
+			[
+				'title'  => 'Learning Some Plugin',
+				'url'    => '#',
+				'plugin' => 'some-plugin/some-plugin.php',
+			],
+		] );
+
+		ob_start();
+		Addons\show_notifications( WP_Screen::get( 'plugins' ) );
+		do_action( 'admin_notices' );
+		$output = ob_get_clean();
+
+		$this->assertContains( 'Learning Some Plugin', $output );
+	}
+
+	/**
+	 * @dataProvider notification_page_provider()
+	 *
+	 * @param string $page     The admin page screen's base.
+	 * @param bool   $expected Should the notification appear on this page?
+	 */
+	public function test_show_notifications_only_shows_on_specific_pages( $page, $expected ) {
+		wp_set_current_user( $this->factory()->user->create( [
+			'role' => 'administrator',
+		] ) );
+
+		update_option( 'wp101-available-series', [
+			[
+				'title'  => 'Learning Some Plugin',
+				'url'    => '#',
+				'plugin' => 'some-plugin/some-plugin.php',
+			],
+		] );
+
+		ob_start();
+		Addons\show_notifications( WP_Screen::get( $page ) );
+		do_action( 'admin_notices' );
+		$output = ob_get_clean();
+
+		if ( $expected ) {
+			$this->assertNotEmpty( $output );
+		} else {
+			$this->assertEmpty( $output );
+		}
+	}
+
+	public function notification_page_provider() {
+		return [
+			'Plugins page'        => [ 'plugins', true ],
+			'WP101 player page'   => [ 'toplevel_page_wp101', true ],
+			'WP101 settings page' => [ 'video-tutorials_page_wp101-settings', true ],
+			'Posts page'          => [ 'edit', false ],
+			'Users page'          => [ 'users', false ],
+		];
+	}
+
+	public function test_show_notifications_checks_capabilities() {
+		wp_set_current_user( $this->factory()->user->create( [
+			'role' => 'subscriber',
+		] ) );
+
+		update_option( 'wp101-available-series', [
+			[
+				'title'  => 'Learning Some Plugin',
+				'url'    => '#',
+				'plugin' => 'some-plugin/some-plugin.php',
+			],
+		] );
+
+		ob_start();
+		Addons\show_notifications( WP_Screen::get( 'plugins' ) );
+		do_action( 'admin_notices' );
+		$output = ob_get_clean();
+
+		$this->assertEmpty( $output );
+	}
+
+	public function test_show_notifications_wont_show_dismissed_notifications() {
+		$this->markTestIncomplete();
+	}
+
+	public function test_show_notifications_can_flatten_multiple_plugins() {
+		wp_set_current_user( $this->factory()->user->create( [
+			'role' => 'administrator',
+		] ) );
+
+		update_option( 'wp101-available-series', [
+			[
+				'title'  => 'First plugin',
+				'url'    => '#',
+				'plugin' => 'first-plugin/first-plugin.php',
+			],
+			[
+				'title'  => 'Second plugin',
+				'url'    => '#',
+				'plugin' => 'second-plugin/second-plugin.php',
+			],
+			[
+				'title'  => 'Third plugin',
+				'url'    => '#',
+				'plugin' => 'third-plugin/third-plugin.php',
+			],
+		] );
+
+		ob_start();
+		Addons\show_notifications( WP_Screen::get( 'plugins' ) );
+		do_action( 'admin_notices' );
+		$output = ob_get_clean();
+
+		$this->assertContains( 'First plugin, Second plugin, and Third plugin', strip_tags( $output ) );
 	}
 }
