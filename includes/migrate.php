4ae7c6a29e7f941e86a0c46a851cb4927826e5f8
Steve Grunwell <sgrunwell@liquidweb.com>
2018-03-14 15:32:51 +0000

    Add a maybe_migrate() function, which will see if a key needs migration and, if so, will exchange it via the /api/key-exchange endpoint

--- includes/migrate.php
+++ includes/migrate.php
@@ -10,6 +10,33 @@ namespace WP101\Migrate;
 use WP101\TemplateTags as TemplateTags;
 
 /**
+ * Apply any necessary migrations.
+ */
+function maybe_migrate() {
+	if ( ! api_key_needs_migration() ) {
+		return;
+	}
+
+	$api = TemplateTags\api();
+	$key = $api->exchange_api_key();
+
+	if ( is_wp_error( $key ) ) {
+		// phpcs:disable WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+		trigger_error( esc_html( $key->get_error_message() ), E_USER_WARNING );
+		// phpcs:enable
+
+		add_action( 'admin_notices', __NAMESPACE__ . '\render_migration_failure_notice' );
+
+		return;
+	}
+
+	update_option( 'wp101_api_key', $key['apiKey'], false );
+	$api->set_api_key( $key['apiKey'] );
+
+	add_action( 'admin_notices', __NAMESPACE__ . '\render_migration_success_notice' );
+}
+
+/**
  * Determine whether the API key option requires migration.
  *
  * @param string $api_key Optional. The current API key to evaluate. Default is null.
@@ -37,4 +64,30 @@ function wp_config_requires_updating() {
 	}
 
 	return api_key_needs_migration( WP101_API_KEY );
+}
+
+/**
+ * Display a notification when an automatic migration has succeeded.
+ */
+function render_migration_success_notice() {
+?>
+
+	<div class="notice notice-success">
+		<p><?php esc_html_e( 'WP101 has automatically upgraded your API key to work with the latest version!', 'wp101' ); ?></p>
+	</div>
+
+<?php
+}
+
+/**
+ * Display a notification when an automatic migration has failed.
+ */
+function render_migration_failure_notice() {
+?>
+
+	<div class="notice notice-error">
+		<p><?php esc_html_e( 'WP101 was unable to automatically migrate your API key for the latest version.', 'wp101' ); ?></p>
+	</div>
+
+<?php
 }
--- tests/test-migrate.php
+++ tests/test-migrate.php
@@ -7,12 +7,65 @@
 
 namespace WP101\Tests;
 
+use PHPUnit\Framework\Error\Warning;
+use WP_Error;
 use WP101\Migrate as Migrate;
 
 /**
  * Tests for migrating from older versions of the WP101 plugin.
  */
 class MigrateTest extends TestCase {
+
+	const LEGACY_API_KEY = 'abcdefghijkl';
+
+	const CURRENT_API_KEY = 'abcdefghijklmnopqrstuvwxyz123456';
+
+	/**
+	 * @after
+	 */
+	public function remove_actions() {
+		remove_all_actions( 'admin_notices' );
+	}
+
+	public function test_maybe_migrate() {
+		$api = $this->mock_api();
+		$api->shouldReceive( 'exchange_api_key' )
+			->once()
+			->andReturn( [
+				'apiKey' => self::CURRENT_API_KEY,
+			] );
+		$this->set_api_key( self::LEGACY_API_KEY );
+
+		Migrate\maybe_migrate();
+
+		$this->assertEquals( self::CURRENT_API_KEY, get_option( 'wp101_api_key' ) );
+		$this->assertEquals( 10, has_action( 'admin_notices', 'WP101\Migrate\render_migration_success_notice' ) );
+	}
+
+	public function test_maybe_migrate_returns_early_if_keys_do_not_require_migration() {
+		$api = $this->mock_api();
+		$api->shouldReceive( 'exchange_api_key' )->never();
+		$this->set_api_key( self::CURRENT_API_KEY );
+
+		Migrate\maybe_migrate();
+
+		$this->assertFalse( has_action( 'admin_notices' ) );
+	}
+
+	public function test_maybe_migrate_handles_wp_errors() {
+		$api = $this->mock_api();
+		$api->shouldReceive( 'exchange_api_key' )
+			->once()
+			->andReturn( new WP_Error( 'some error' ) );
+		$this->set_api_key( self::LEGACY_API_KEY );
+
+		$this->expectException( Warning::class );
+
+		Migrate\maybe_migrate();
+
+		$this->assertEquals( self::LEGACY_API_KEY, get_option( 'wp101_api_key' ) );
+		$this->assertTrue( has_action( 'admin_notices', 'WP101\Migrate\render_migration_failure_notice' ) );
+	}
 
 	/**
 	 * @dataProvider api_key_provider()
@@ -32,8 +85,8 @@ class MigrateTest extends TestCase {
 
 	public function api_key_provider() {
 		return [
-			'Legacy API key'  => [ 'abcdefghijkl', true ],
-			'Current API key' => [ 'abcdefghijklmnopqrstuvwxyz123456', false ],
+			'Legacy API key'  => [ self::LEGACY_API_KEY, true ],
+			'Current API key' => [ self::CURRENT_API_KEY, false ],
 		];
 	}
 
@@ -46,7 +99,7 @@ class MigrateTest extends TestCase {
 	 * @requires extension runkit
 	 */
 	public function test_wp_config_requires_updating_with_old_constant() {
-		define( 'WP101_API_KEY', 'abcdefghijkl' );
+		define( 'WP101_API_KEY', self::LEGACY_API_KEY );
 
 		$this->assertTrue( Migrate\wp_config_requires_updating() );
 	}
@@ -55,8 +108,24 @@ class MigrateTest extends TestCase {
 	 * @requires extension runkit
 	 */
 	public function test_wp_config_requires_updating_with_new_constant() {
-		define( 'WP101_API_KEY', 'abcdefghijklmnopqrstuvwxyz123456' );
+		define( 'WP101_API_KEY', self::CURRENT_API_KEY );
 
 		$this->assertFalse( Migrate\wp_config_requires_updating() );
+	}
+
+	public function test_render_migration_success_notice() {
+		ob_start();
+		Migrate\render_migration_success_notice();
+		$output = ob_get_clean();
+
+		$this->assertContainsSelector( 'div.notice.notice-success', $output );
+	}
+
+	public function test_render_migration_failure_notice() {
+		ob_start();
+		Migrate\render_migration_failure_notice();
+		$output = ob_get_clean();
+
+		$this->assertContainsSelector( 'div.notice.notice-error', $output );
 	}
 }
